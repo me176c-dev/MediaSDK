2f255f1a7b56762852fc6ab501cdf380cc4ae22c
lambdadroid <lambdadroid@gmail.com>
2018-08-14 20:32:08 +0200

    android: Export libva headers in libmfx_headers
    This fixes the build for various modules that otherwise fail to
    find the libva headers on their include path.

--- android/Android.mk
+++ android/Android.mk
@@ -6,5 +6,6 @@ LOCAL_MODULE := libmfx_headers
 LOCAL_EXPORT_C_INCLUDE_DIRS :=  \
     $(MFX_HOME)/api/include \
     $(MFX_HOME)/android/include
+LOCAL_EXPORT_SHARED_LIBRARY_HEADERS := libva
 
 include $(BUILD_HEADER_LIBRARY)
